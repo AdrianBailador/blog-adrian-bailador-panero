a7f515340128e635b07fc955e6feb45c6f1f9e11
Adrian Bailador <adrianbailador@hotmail.com>
2025-01-26 18:05:33 +0000

    updates , and . and article

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default function Home({ articles }) {
         <section className="pt-16">
           <h2 className="text-4xl mb-4">About Me</h2>
           <p className="mb-5">
-            I am a passionate and dedicated .NET Software Engineer with extensive experience in application development using .NET Core, .NET Framework, Azure, React, Java, and SQL Server. Throughout my career, I have demonstrated the ability to design and optimize SQL Server databases, create intuitive user interfaces using React and JavaScript, and efficiently manage code version control with Git.
+            I am a passionate and dedicated .NET Software Engineer with extensive experience in application development using .NET Core, .NET Framework, Azure, React, Java and SQL Server. Throughout my career, I have demonstrated the ability to design and optimize SQL Server databases, create intuitive user interfaces using React and JavaScript, and efficiently manage code version control with Git.
             <br />
             <br />
-            I possess solid knowledge of the entire software development lifecycle, including requirements analysis, application implementation, testing, and maintenance. I am always seeking opportunities to apply and expand my technical skills while delivering value and innovative solutions to software development teams.
+            I possess solid knowledge of the entire software development lifecycle, including requirements analysis, application implementation, testing and maintenance. I am always seeking opportunities to apply and expand my technical skills while delivering valuable and innovative solutions to software development teams.
             <br />
             <br />
-            My long-term goal is to become a Software Architect, and I am continuously learning and improving my skills to achieve this objective. I strongly believe in the potential of technology to enhance business operations, and I am motivated by the opportunity to contribute to the creation of solutions that make a meaningful impact.
+            My long-term goal is to become a Software Architect. I am continuously learning and improving my skills to achieve this objective. I strongly believe in the potential of technology to enhance business operations, and I am motivated by the opportunity to contribute to the creation of solutions that make a meaningful impact.
           </p>
         </section>
         <section className="pt-16">
