2ec5a6c434e355d443e1cb27ae4dfef0875f625c
Adrian Bailador <adrianbailador@hotmail.com>
2025-02-03 11:38:43 +0000

    Improve page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { Sun, Moon } from "lucide-react"; 
+import { Sun, Moon } from "lucide-react";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -8,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-    // Cambiar la clase 'dark' en el documento
+    // dark
     if (newTheme === "dark") {
       document.documentElement.classList.add("dark");
     } else {
@@ -28,8 +29,16 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div>
+      <Head>
+        <title>Mi Blog - Adrian Bailador Panero</title> 
+        <meta name="description" content="Blog personal de Adrian Bailador Panero" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+        <link rel="icon" href="/favicon.ico" /> 
+      </Head>
+
       <button
         onClick={toggleTheme}
+        aria-label={`Switch to ${theme === "light" ? "dark" : "light"} mode`}
         className="fixed top-4 right-4 p-2 bg-gray-200 dark:bg-gray-700 rounded-full transition-colors"
       >
         {theme === "light" ? (
--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,28 @@ import { pick } from "@contentlayer/client";
 import Article from "components/article";
 import { Linkedin, Github } from "lucide-react";
 import { SiX } from "react-icons/si";
+import Head from 'next/head';
 
 export default function Home({ articles }) {
   return (
     <div className="px-8">
+      <Head>
+        <title>Adrian Bailador Panero | Senior Software Engineer</title>
+        <html lang="en" />
+      </Head>
+
       <main className="max-w-7xl mx-auto pt-32 pb-40">
-      <div className="flex items-center gap-4">
-  <img
-    src="/adrian.jpg"
-    alt="Adrian Bailador"
-    className="w-24 h-24 rounded-full border-4 border-gray-300 object-cover"
-  />
-  <div className="flex flex-col">
-    <h1 className="text-4xl md:text-6xl leading-tight">Adrian Bailador Panero</h1>
-    <p className="font-bold text-lg text-gray-600">Senior Software Engineer | .NET | C# | Azure | JS</p>
-  </div>
-</div>
-
-
-
+        <div className="flex items-center gap-4">
+          <img
+            src="/adrian.jpg"
+            alt="Adrian Bailador"
+            className="w-24 h-24 rounded-full border-4 border-gray-300 object-cover"
+          />
+          <div className="flex flex-col">
+            <h1 className="text-4xl md:text-6xl leading-tight">Adrian Bailador Panero</h1>
+            <p className="font-bold text-lg text-gray-600">Senior Software Engineer | .NET | C# | Azure | JS</p>
+          </div>
+        </div>
 
         <section className="pt-16">
           <h2 className="text-4xl mb-4">About Me</h2>
@@ -35,6 +38,7 @@ export default function Home({ articles }) {
             My long-term goal is to become a Software Architect. I am continuously learning and improving my skills to achieve this objective. I strongly believe in the potential of technology to enhance business operations, and I am motivated by the opportunity to contribute to the creation of solutions that make a meaningful impact.
           </p>
         </section>
+
         <section className="pt-16">
           <h2 className="text-4xl mb-4">Articles</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9 w-full">
@@ -54,6 +58,7 @@ export default function Home({ articles }) {
               href="https://linkedin.com/in/adrianbailadorpanero"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="LinkedIn Profile"
               className="flex items-center space-x-2 text-blue-600 hover:underline"
             >
               <Linkedin className="w-6 h-6" />
@@ -62,6 +67,7 @@ export default function Home({ articles }) {
               href="https://x.com/DotNetDevABP"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="X Profile"
               className="flex items-center space-x-2 text-blue-400 hover:underline"
             >
               <SiX className="w-6 h-6" />
@@ -70,6 +76,7 @@ export default function Home({ articles }) {
               href="https://github.com/AdrianBailador"
               target="_blank"
               rel="noopener noreferrer"
+              aria-label="GitHub Profile"
               className="flex items-center space-x-2 text-gray-300 hover:underline"
             >
               <Github className="w-6 h-6" />
