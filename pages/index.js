9274a9a9d2db366a9ed80366d03ee0a01e7aa915
Adrian Bailador <adrianbailador@hotmail.com>
2025-02-13 18:55:29 +0000

    Update pages, v2

--- pages/index.js
+++ pages/index.js
@@ -121,6 +121,22 @@ export default function Home({ articles }) {
             </button>
           </div>
         </section>
+
+        <section className="pt-16">
+          <h2 className="text-4xl mb-4">Follow Me</h2>
+          <p className="text-lg mb-4">Connect with me on my social networks:</p>
+          <div className="flex space-x-6">
+            <a href="https://linkedin.com/in/adrianbailadorpanero" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile" className="flex items-center space-x-2 text-blue-600 hover:underline">
+              <Linkedin className="w-6 h-6" />
+            </a>
+            <a href="https://x.com/DotNetDevABP" target="_blank" rel="noopener noreferrer" aria-label="X Profile" className="flex items-center space-x-2 text-blue-400 hover:underline">
+              <SiX className="w-6 h-6" />
+            </a>
+            <a href="https://github.com/AdrianBailador" target="_blank" rel="noopener noreferrer" aria-label="GitHub Profile" className="flex items-center space-x-2 text-gray-300 hover:underline">
+              <Github className="w-6 h-6" />
+            </a>
+          </div>
+        </section>
       </main>
     </div>
   );
