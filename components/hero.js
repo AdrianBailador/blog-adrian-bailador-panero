8234b81114f084cbf0fbcb6780309839eb9b16fd
Adrian Bailador <adrianbailador@hotmail.com>
2025-02-22 16:28:17 +0000

    image

--- components/hero.js
+++ components/hero.js
@@ -3,13 +3,14 @@ import Image from 'next/image';
 export default function Hero() {
   return (
     <div className="flex items-center gap-4">
-      <Image
-        src="/adrian.jpg"
-        alt="Adrian Bailador"
-        className="w-24 h-24 rounded-full border-4 border-gray-300 object-cover"
-        width={96}  
-        height={96} 
-      />
+  <Image
+  src="/adrian.jpg"
+  alt="Adrian Bailador"
+  className="w-24 h-24 rounded-full border-4 border-gray-300"
+  width={96}
+  height={96}
+  unoptimized
+/>
       <div className="flex flex-col">
         <h1 className="text-4xl md:text-6xl leading-tight">Adrian Bailador Panero</h1>
         <p className="font-bold text-lg text-gray-600">Senior Software Engineer | .NET | C# | Azure | JS</p>
